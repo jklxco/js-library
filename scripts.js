8e571d6f61c0cb8bfceb0a62aa7c05f9877fe3cf
jklxco <jack.a.coleman@hotmail.com>
2022-05-07 21:26:37 +0700

    Add read toggle and remove book button

--- scripts.js
+++ scripts.js
@@ -1,69 +1,100 @@
 let myLibrary = [];
 
 function Book(author, title, numberOfPages, readStatus) {
-  // the constructor...
-  this.author = author;
-  this.title = title;
-  this.numberOfPages = numberOfPages;
-  this.readStatus = readStatus;
+    this.author = author;
+    this.title = title;
+    this.numberOfPages = numberOfPages;
+    this.readStatus = readStatus;
 }
 
-function addBookToLibrary() {
-    // author, title, number of pages, whether it’s been read
-    /*let author = prompt('Author: ');
-    let title = prompt('Book Title: ');
-    let numberOfPages = prompt('Number of pages: ');
-    let readStatus = prompt('Read? ');
-    */
-    author = "Jesus Christ";
-    title = "The Bible";
-    numberOfPages = "123";
-    readStatus = "No";
+Book.prototype.toggleRead = function() {
+    if (this.readStatus === 'Yes') {
+        this.readStatus = 'No';
+    } else {
+        this.readStatus = 'Yes';
+    }
+};
 
-    const book = new Book(author, title, numberOfPages, readStatus);
+function addBookToLibrary() {
+
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').value;
+    const book = new Book(author, title, pages, read);
     myLibrary.push(book);
+    
 }
 
 function addBookToDOM() {
-    console.log(myLibrary);
-    for (const book in myLibrary) {
+    document.getElementById("books").innerHTML = '';
+    for (i = 0 ; i < myLibrary.length ; i ++) {
         let div = document.createElement("div");
         div.classList.add("card");
-        
+        div.setAttribute('id', 'card_'+i);
         let para = document.createElement("p");
-        para.innerHTML = "Title: " + this.title;
+        para.innerHTML = "Title: " + myLibrary[i].title;
+        div.appendChild(para)
+        para = document.createElement("p");
+        para.innerHTML = "Author: " + myLibrary[i].author;
+        div.appendChild(para)
+        para = document.createElement("p");
+        para.innerHTML = "Page Length: " + myLibrary[i].numberOfPages;
+        div.appendChild(para)
+        para = document.createElement("p");
+        para.innerHTML = "Read: " + myLibrary[i].readStatus;
         div.appendChild(para)
 
-        para = document.createElement("p");
-        para.innerHTML = "Author: " + this.author;
-        div.appendChild(para)
+        let btnRead = document.createElement("button");
+        btnRead.classList.add("btn-toggle-read");
+        btnRead.innerHTML = "Toggle read";
+        div.appendChild(btnRead);
 
-        para = document.createElement("p");
-        para.innerHTML = "Page Length: " + this.numberOfPages;
-        div.appendChild(para)
+        btnRead.addEventListener('click', (e) => {
+            const index = parseInt(e.path[1].id.slice(-1));
+            console.log(e);
+            console.log(myLibrary[index]);
+            myLibrary[index].toggleRead();
+        });
 
-        para = document.createElement("p");
-        para.innerHTML = "Read: " + this.readStatus;
-        div.appendChild(para)
+        let btnRemoveBook = document.createElement("button");
+        btnRemoveBook.classList.add("btn-remove-book");
+        btnRemoveBook.innerHTML = "x";
+        div.appendChild(btnRemoveBook);
+
+        btnRemoveBook.addEventListener('click', (e) => {
+            e.path[1].remove();
+            const arrayIndex = parseInt(e.path[1].id.slice(-1));
+            myLibrary.splice(arrayIndex, 1);
+        });
 
         document.getElementById("books").appendChild(div);
+
     }
 }
 
-addBookToLibrary();
-addBookToLibrary();
-addBookToDOM();
+
+
+const btnSubmit = document.getElementById('btn-submit');
+btnSubmit.addEventListener('click', () => {
+    //Add validation
+    addBookToLibrary() 
+    addBookToDOM();
+    document.getElementById("new-book-form").style.display="none";
+});
+
+const btnCancel = document.getElementById('btn-cancel');
+btnCancel.addEventListener('click', () => {
+    document.getElementById("new-book-form").style.display="none";
+});
+
+const btnNewBook = document.getElementById('new-book');
+btnNewBook.addEventListener('click', () => {
+    document.getElementById("new-book-form").style.display="flex";
+});
+
 
 /*  
-    * Add a “NEW BOOK” button that brings up a form allowing users to input the
-        details for the new book: author, title, number of pages, whether it’s
-        been read and anything else you might want.
-    
-    * Add a button on each book’s display to remove the book from the library.
-        You will need to associate your DOM elements with the actual book
-        objects in some way. One easy solution is giving them a data-attribute
-        that corresponds to the index of the library array.
-
     * Add a button on each book’s display to change its read status.
         To facilitate this you will want to create the function that
         toggles a book’s read status on your Book prototype instance.
